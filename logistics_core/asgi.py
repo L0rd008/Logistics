10079ec5c70deafa12ccc304e20ffc2597ee7ef1
Kevin Sanjula <kevinxsanjula@gmail.com>
2025-04-22 10:36:19 +0530

    not using camel case (#3)
    * not using camel case
    
    * .

--- logistics_core/asgi.py
+++ logistics_core/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for logisticsCore project.
+ASGI config for logistics_core project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'logisticsCore.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'logistics_core.settings')
 
 application = get_asgi_application()
--- logistics_core/urls.py
+++ logistics_core/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for logisticsCore project.
+URL configuration for logistics_core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
