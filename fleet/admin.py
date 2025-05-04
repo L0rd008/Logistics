090cfb117dde7262e36011f6f7f9c57550a1bf0c
Ke-vin-S <kevinxsanjula@gmail.com>
2025-05-04 13:30:23 +0530

    split models into core and extended models so those can be developed separately and  enabled when needed

--- fleet/admin.py
+++ fleet/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Vehicle, MaintenanceRecord, FuelRecord, TripRecord, VehicleLocation
+from django.conf import settings
+from .models import Vehicle, VehicleLocation
 
 @admin.register(Vehicle)
 class VehicleAdmin(admin.ModelAdmin):
@@ -24,57 +25,6 @@ class VehicleAdmin(admin.ModelAdmin):
     )
 
 
-@admin.register(MaintenanceRecord)
-class MaintenanceRecordAdmin(admin.ModelAdmin):
-    list_display = ('vehicle', 'maintenance_type', 'status', 'scheduled_date', 'completion_date')
-    list_filter = ('maintenance_type', 'status', 'scheduled_date')
-    search_fields = ('vehicle__vehicle_id', 'description')
-    readonly_fields = ('created_at', 'updated_at')
-    raw_id_fields = ('vehicle',)
-    date_hierarchy = 'scheduled_date'
-
-
-@admin.register(FuelRecord)
-class FuelRecordAdmin(admin.ModelAdmin):
-    list_display = ('vehicle', 'refuel_date', 'amount', 'cost', 'odometer_reading')
-    list_filter = ('refuel_date',)
-    search_fields = ('vehicle__vehicle_id', 'location_name', 'notes')
-    readonly_fields = ('created_at',)
-    raw_id_fields = ('vehicle',)
-    date_hierarchy = 'refuel_date'
-
-
-@admin.register(TripRecord)
-class TripRecordAdmin(admin.ModelAdmin):
-    list_display = ('vehicle', 'start_time', 'end_time', 'distance', 'driver_name')
-    list_filter = ('start_time',)
-    search_fields = ('vehicle__vehicle_id', 'driver_name', 'purpose', 'notes')
-    readonly_fields = ('created_at', 'updated_at', 'distance', 'duration')
-    raw_id_fields = ('vehicle',)
-    date_hierarchy = 'start_time'
-    fieldsets = (
-        ('Trip Information', {
-            'fields': ('vehicle', 'driver_name', 'purpose', 'notes')
-        }),
-        ('Timing', {
-            'fields': ('start_time', 'end_time', 'duration')
-        }),
-        ('Distance', {
-            'fields': ('start_odometer', 'end_odometer', 'distance')
-        }),
-        ('Location', {
-            'fields': (
-                'start_latitude', 'start_longitude', 
-                'end_latitude', 'end_longitude'
-            )
-        }),
-        ('Timestamps', {
-            'fields': ('created_at', 'updated_at'),
-            'classes': ('collapse',)
-        })
-    )
-
-
 @admin.register(VehicleLocation)
 class VehicleLocationAdmin(admin.ModelAdmin):
     list_display = ('vehicle', 'timestamp', 'latitude', 'longitude', 'speed')
@@ -82,3 +32,55 @@ class VehicleLocationAdmin(admin.ModelAdmin):
     search_fields = ('vehicle__vehicle_id',)
     raw_id_fields = ('vehicle',)
     date_hierarchy = 'timestamp'
+
+# Conditionally register extended models
+if getattr(settings, 'ENABLE_FLEET_EXTENDED_MODELS', False):
+    from .models import MaintenanceRecord, FuelRecord, TripRecord
+
+    @admin.register(MaintenanceRecord)
+    class MaintenanceRecordAdmin(admin.ModelAdmin):
+        list_display = ('vehicle', 'maintenance_type', 'status', 'scheduled_date', 'completion_date')
+        list_filter = ('maintenance_type', 'status', 'scheduled_date')
+        search_fields = ('vehicle__vehicle_id', 'description')
+        readonly_fields = ('created_at', 'updated_at')
+        raw_id_fields = ('vehicle',)
+        date_hierarchy = 'scheduled_date'
+
+    @admin.register(FuelRecord)
+    class FuelRecordAdmin(admin.ModelAdmin):
+        list_display = ('vehicle', 'refuel_date', 'amount', 'cost', 'odometer_reading')
+        list_filter = ('refuel_date',)
+        search_fields = ('vehicle__vehicle_id', 'location_name', 'notes')
+        readonly_fields = ('created_at',)
+        raw_id_fields = ('vehicle',)
+        date_hierarchy = 'refuel_date'
+
+    @admin.register(TripRecord)
+    class TripRecordAdmin(admin.ModelAdmin):
+        list_display = ('vehicle', 'start_time', 'end_time', 'distance', 'driver_name')
+        list_filter = ('start_time',)
+        search_fields = ('vehicle__vehicle_id', 'driver_name', 'purpose', 'notes')
+        readonly_fields = ('created_at', 'updated_at', 'distance', 'duration')
+        raw_id_fields = ('vehicle',)
+        date_hierarchy = 'start_time'
+        fieldsets = (
+            ('Trip Information', {
+                'fields': ('vehicle', 'driver_name', 'purpose', 'notes')
+            }),
+            ('Timing', {
+                'fields': ('start_time', 'end_time', 'duration')
+            }),
+            ('Distance', {
+                'fields': ('start_odometer', 'end_odometer', 'distance')
+            }),
+            ('Location', {
+                'fields': (
+                    'start_latitude', 'start_longitude',
+                    'end_latitude', 'end_longitude'
+                )
+            }),
+            ('Timestamps', {
+                'fields': ('created_at', 'updated_at'),
+                'classes': ('collapse',)
+            })
+        )
